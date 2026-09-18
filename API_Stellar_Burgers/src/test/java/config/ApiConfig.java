234a4b4d6e5fe950cbeb65cd157eff78b27a278f
Елена Чефранова <chefranovaev@gmail.com>
2026-09-18 10:46:00 +0300

    Add files via upload

---
+++ API_Stellar_Burgers/src/test/java/config/ApiConfig.java
@@ -0,0 +1,15 @@
+package config;
+
+public class ApiConfig {
+    public static final String BASE_URL = "https://stellarburgers.education-services.ru/";
+
+    public static final String USER_REGISTER_PATH = "/api/auth/register";
+    public static final String USER_LOGIN_PATH = "/api/auth/login";
+    public static final String USER_DATA_PATH = "/api/auth/user";
+
+    public static final String ORDERS_PATH = "/api/orders";
+
+    public static final String VALID_INGREDIENT = "61c0c5a71d1f82001bdaaa6d";
+    public static final String INVALID_INGREDIENT = "invalid_hash_12345";
+}
+
